14b751da7ac4507a679214adb85ce3eb4e85e3d2
Franz Bonaparta <96510220+franeuz@users.noreply.github.com>
2026-02-14 10:48:50 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+# Qtile minimal config – Franz
+
+from lab.settings import *
+from lab.keys import keys
+from lab.groups import groups, group_keys
+from lab.layouts import layouts
+from lab.rules import *
+from lab.screens import screens
+import lab.hooks
+
+keys.extend(group_keys())
+
+wmname = "Qtile"
