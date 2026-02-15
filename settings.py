6b369ab271e2d83dd91ce0dad57032c9696eb6fb
Franz Bonaparta <96510220+franeuz@users.noreply.github.com>
2026-02-15 13:08:22 -0400

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,28 @@
+# lab/settings.py
+# Normalización
+
+mod = "mod4"
+emacs = "emacs"
+terminal = "alacritty"
+browser = "vivaldi"
+launcher = "rofi -show drun"
+launcher_window = "rofi -show window"
+
+font = "terminus-font"
+
+colors = {
+    "bg": "#0a3033",
+    "focus": "#dadbdd",
+    "inactive_bg": "#0a3033",
+    "text": "#23a1ad",
+    "inactive_text": "#14636a",
+    "urgent": "#23a1ad",
+    "blue": "#2c5054",
+}
+
+gaps = {
+    "inner": 0,
+    "outer": -18,
+}
+
+wallpaper = "/home/franz/Imágenes/fondos_de_pantalla/arquetipo.png"
